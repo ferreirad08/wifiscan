f6e6faf8689eded3389616591a21ee03518ac2d1
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2022-12-03 20:29:24 -0300

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,8 +1,16 @@
-const { getInterface } = require('../lib');
+const { getInterface, scanNetworks } = require('../lib');
 
 describe('Tests of getInterface function', () => {
   it('type', () => {
     const iface = getInterface();
     expect(typeof iface).toEqual('string');
+  });
+});
+
+describe('Tests of scanNetworks function', () => {
+  it('type', () => {
+    const iface = getInterface();
+    const networks = scanNetworks(iface);
+    expect(networks).toBeInstanceOf(Object);
   });
 });
