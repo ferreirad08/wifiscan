d937d628e4d8493b5b82a23d2845049eceb09ac7
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2022-12-03 20:03:56 -0300

    Update lib.test.js

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -3,6 +3,6 @@ const lib = require('../lib/index.js');
 describe('Tests of getInterface function', () => {
   it('type', () => {
     const iface = lib.getInterface();
-    expect(iface).toBeInstanceOf(String);
+    expect(iface).toBeInstanceOf(string);
   });
 });
