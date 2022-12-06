a8b8b6fc115a813be6704eb0628d9973988cfa1c
David Ferreira <46223447+ferreirad08@users.noreply.github.com>
2022-12-06 01:45:23 -0400

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,5 @@
-const { getInterface, scanNetworks } = require('../lib');
+const { getInterface, scanNetworks, wifiAnalyzer } = require('../lib');
+const { execSync } = require('child_process');
 
 const validInterfaces = [
   'eth0',
@@ -21,6 +22,14 @@ describe('scanNetworks function', () => {
   it('output is object', () => {
     const iface = getInterface();
     const networks = scanNetworks(iface);
+    expect(networks).toBeInstanceOf(Object);
+  });
+});
+
+describe('wifiAnalyzer function', () => {
+  it('output of scanNetworks endpoint is object', () => {
+    wifiAnalyzer(3000);
+    networks = JSON.parse(execSync(`curl http://localhost:3000/scanNetworks`));
     expect(networks).toBeInstanceOf(Object);
   });
 });
